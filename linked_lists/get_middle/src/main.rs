3a456a0b469c939eb28b67c81617054601ba4c9d
Everton C Prado Jr <ecpj.prado@gmail.com>
2025-04-11 11:57:35 -0300

    Middle of the Linked List: Fast and Slow

--- linked_lists/get_middle/src/main.rs
+++ linked_lists/get_middle/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::repeat;
+// 876. Middle of the Linked List: https://leetcode.com/problems/middle-of-the-linked-list
 
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
@@ -12,10 +12,15 @@ impl ListNode {
         ListNode { next: None, val }
     }
     fn push(&mut self, val: i32) {
-        let new_node = Box::new(ListNode::new(val));
+        let new_node = ListNode::new(val);
 
-        let out = std::mem::replace(self, *new_node);
+        let out = std::mem::replace(self, new_node);
         self.next = Some(Box::new(out));
+    }
+}
+impl Drop for ListNode {
+    fn drop(&mut self) {
+        println!("dropped: {:?}", self)
     }
 }
 
@@ -35,36 +40,88 @@ fn main() {
         head.push(i);
     }
 
-    println!("{:?}\n", head);
-    println!("{:?}", Solution::middle_node(Some(Box::new(head))));
+    println!("{:?}", Solution::middle_node3(Some(Box::new(head))));
 }
+
 struct Solution {}
 impl Solution {
+    // Based on Theory
     // Simple approach: count the nodes, then return the upper middle node
     pub fn middle_node(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        let mut p = head.as_deref();
-        let mut size = 0;
-
-        while p != None {
-            p.map(|node| {
-                size += 1;
-                p = node.next.as_deref()
-            });
+        let mut length = 0;
+        {
+            let mut current = head.as_ref();
+            while let Some(node) = current {
+                length += 1;
+                current = node.next.as_ref();
+            }
         }
 
-        let mid = if size & 1 == 1 {
-            size / 2 + 1 // Odd
-        } else {
-            size / 2 // Pair
-        };
+        let mid = length / 2;
 
-        while size > mid {
-            if let Some(node) = head.take() {
-                head = node.next;
-                size -= 1;
+        for _ in 0..mid {
+            if let Some(mut node) = head.take() {
+                head = node.next.take();
             }
         }
 
         head
+    }
+
+    // Slow and Fast approach: My translated version of the provided solution
+    pub fn middle_node2(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut slow = head.as_ref();
+        let mut fast = head.as_ref();
+
+        loop {
+            // After this, I saw a lot of versions of this solution and feel better with this "ugly" code
+            // because there are some really much uglier versions of this section...
+            fast.map(|node| fast = node.next.as_ref());
+            let next = fast.map(|node| fast = node.next.as_ref());
+
+            if next == None {
+                break;
+            }
+
+            slow.map(|node| slow = node.next.as_ref());
+        }
+
+        slow.cloned()
+    }
+
+    // fifty_four_k's "fast and slow" translated version using unsafe code
+    // Drop says: missing some nodes
+    // Miri says: memory leaked
+    // https://leetcode.com/problems/middle-of-the-linked-list/solutions/2878748/rust-2-pointers-safe-and-unsafe-with-no-cloning/
+    pub fn middle_node3(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        unsafe {
+            let mut s = Box::into_raw(head?);
+            let mut f = s.as_ref();
+
+            while f.is_some() && f.unwrap().next.is_some() {
+                f = f?.next.as_ref()?.next.as_deref();
+                s = Box::into_raw((*s).next.take()?);
+            }
+
+            Some(Box::from_raw(s))
+        }
+    }
+
+    // GPT's Reasoning-mode unsafe version
+    // Drop says: "I see all nodes"
+    // Miri says: Undefined Behavior
+    // I tried to give the Miri alert to GPT, but they took 5 minutes to give me a solution that doesn't even compile
+    pub fn middle_node4(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut slow: *mut Option<Box<ListNode>> = &mut head;
+        let mut fast: *mut Option<Box<ListNode>> = &mut head;
+
+        unsafe {
+            while (*fast).is_some() && (*fast).as_ref().unwrap().next.is_some() {
+                slow = &mut (*slow).as_mut().unwrap().next as *mut _;
+                fast = &mut (*fast).as_mut().unwrap().next.as_mut().unwrap().next as *mut _;
+            }
+
+            std::mem::take(&mut *slow)
+        }
     }
 }
