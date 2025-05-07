ce4deca6ac88a40cb8df0e9bd4fe19b1fd90aeec
Everton C Prado Jr <ecpj.prado@gmail.com>
2025-05-07 09:39:26 -0300

    Logic Building: Check power

---
+++ geeks_for_geeks/logic_building/src/easy/check_power.rs
@@ -0,0 +1,44 @@
+pub struct Solution {}
+impl Solution {
+    pub fn is_power(x: u32, y: u32) -> bool {
+        if x == 1 {
+            return y == 1;
+        }
+
+        let mut power = 1;
+        while power < y {
+            power *= x;
+        }
+
+        power == y
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example_01() {
+        let x = 10;
+        let y = 1;
+
+        assert!(Solution::is_power(x, y));
+    }
+
+    #[test]
+    fn example_02() {
+        let x = 10;
+        let y = 1000;
+
+        assert!(Solution::is_power(x, y));
+    }
+
+    #[test]
+    fn example_03() {
+        let x = 10;
+        let y = 1001;
+
+        assert!(!Solution::is_power(x, y));
+    }
+}
--- geeks_for_geeks/logic_building/src/easy/mod.rs
+++ geeks_for_geeks/logic_building/src/easy/mod.rs
@@ -1,3 +1,4 @@
+pub mod check_power;
 pub mod digit_sum;
 pub mod prime_number;
 pub mod reverse_digits;
